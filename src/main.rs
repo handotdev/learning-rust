c5a48c7383781bbd83003b8efadd0e2aa8b25194
Han Wang <hyw2@cornell.edu>
2022-01-01 13:41:02 -0500

    Follow up to arrays.rs intro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-mod print;
+// mod print;
+// mod vars;
+// mod types;
+// mod strings;
+// mod tuples;
+mod arrays;
 
 fn main() {
-    print::run();
+    // print::run();
+    // types::run();
+    // strings::run();
+    // tuples::run();
+    arrays::run();
 }
